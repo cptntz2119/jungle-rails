675916fe48eeb3e57437eb2afa1092d5e72d397a
Dong Zhang <dongzhang@Dongs-MacBook-Pro.local>
2022-04-06 18:23:27 -0700

    dispaly count for dashborad

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["AUTH_NAME"], password: ENV["AUTH_PASS"]
+
   def show
+    @products = Product.count(:all) 
+    @categories = Category.count(:all) 
   end
+
 end
