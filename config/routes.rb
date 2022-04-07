c877da761e5c0224619b7a0179816d924e588e18
Dong Zhang <dongzhang@Dongs-MacBook-Pro.local>
2022-04-07 15:52:10 -0700

    add routes and generate users controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,14 @@ Rails.application.routes.draw do
   resources :about, only: [:index]
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
-
   resources :orders, only: [:create, :show]
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   namespace :admin do
     root to: 'dashboard#show'
